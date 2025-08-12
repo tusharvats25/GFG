b08f61b87bb3352e8f33c1991b0b25221ed3071f
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-08-12 14:09:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -2,30 +2,11 @@ class Solution {
   public:
     int getLPSLength(string &s) {
         // code here
-        vector<int>lps(s.size(),0);
-        int pre=0,suf=1;
-        while(suf<s.size())
-        {    //Match
-            if(s[pre]==s[suf])
-            {
-                lps[suf]=pre+1;
-                pre++,suf++;
-            }
-            
-            //Not Match
-            else
-            {
-                if(pre==0)
-                {
-                     lps[suf]=0;
-                     suf++;
-                }
-                else
-                {
-                    pre=lps[pre-1];
-                }
-            }
-        }
-        return lps[s.size()-1];
+        vector<int> lps(s.size(), 0);
+for (int pre = 0, suf = 1; suf < s.size();)
+    if (s[pre] == s[suf]) lps[suf++] = ++pre;
+    else if (pre) pre = lps[pre - 1];
+    else lps[suf++] = 0;
+return lps[s.size() - 1];
     }
 };
