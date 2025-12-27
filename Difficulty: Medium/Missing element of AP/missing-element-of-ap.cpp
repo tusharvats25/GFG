3c9311b18619727b2d6635f8e9255b1ce86f7c4a
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-12-27 18:56:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
+++ Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
@@ -1,33 +1,27 @@
+// User function template for C++
+
 class Solution {
   public:
-
     int findMissing(vector<int> &arr) {
+        // code here
         int n=arr.size();
         int diff;
-        int diff1=arr[1]-arr[0],diff2=arr[n-1]-arr[n-2],
-            diff3=(arr[n-1]-arr[0])/n;
+        int diff1=arr[1]-arr[0];
+        int diff2=arr[n-1]-arr[n-2];
+        int diff3=(arr[n-1]-arr[0])/n;
         if(diff1==diff2)
-            diff=diff1;
+        diff=diff1;
         else if(diff1==diff3)
-            diff=diff1;
+        diff=diff1;
         else
-            diff=diff2;
-        if(diff==0)
-           return arr[0];
-        int start=0,end=n-1;
-        while(start<=end) {
-            int mid=(start+end)/2;
-            // if mid == (nth position of element in AP)-1
-            // the missing element will exist in right half
-            if ((arr[mid]-arr[0])/diff==mid)
-                start=mid+1;
-            // the missing element will exist in left half
-            else
-                end=mid-1;
+        diff=diff2;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i+1]!=arr[i]+diff)
+            return arr[i]+diff;
         }
-
-        // after breaking out of binary search loop the missing element
-        // will exist between high and low
-        return arr[end]+diff;
+        
+        return arr[n-1]+diff;
     }
 };
