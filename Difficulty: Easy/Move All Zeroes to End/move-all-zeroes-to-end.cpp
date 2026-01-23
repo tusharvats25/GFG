618d1b48daadbbc4b32c3864c5ed9d8baea01b62
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2026-01-23 17:25:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -5,13 +5,12 @@ class Solution {
         int j=0;
         for(int i=0;i<arr.size();i++)
         {
-            
             if(arr[i]!=0)
             {
-            swap(arr[i],arr[j]);
-            j++;
+                swap(arr[i],arr[j]);
+                j++;
             }
-            
+          
         }
     }
 };
